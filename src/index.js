7f7266fc3d5afae8ddcea818fc778efcc91fd004
Nicolas Posa <nicoposa57@gmail.com>
2020-09-13 23:04:39 -0300

    Comienzo del hero

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './Global.css'
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('app')
 
 ReactDOM.render(
-<Badge 
-  firstName= "Nicolás"
-  lastName = "Posa"
-  jobTittle= "Frontend Engineer"
-  twitter= "nicoposa1"
-  avatarUrl= "https://es.gravatar.com/userimage/192994403/013c21725ee2b66b21941e678d3bb965.jpg"
+<BadgeNew
+  
 />, 
 container)
