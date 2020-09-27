465b6086a1f48e3134af3776af48c5ea9a598162
Nicolas Posa <nicoposa57@gmail.com>
2020-09-27 19:48:49 -0300

    fixes

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./styles/Loader.css'
+
+class Loader extends React.Component {
+  render () {
+    return (
+    <div className="loadingio-spinner-dual-ball-xufup796s4">
+      <div className="ldio-681o1unozd7">
+        <div></div><div></div><div></div>
+      </div>
+    </div>
+    )
+  }
+
+}
+
+export default Loader
