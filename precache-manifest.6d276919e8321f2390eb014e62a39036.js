813e162a6988e09581b84e9894518f3dd62714de
root <root@DESKTOP-K0MI401.localdomain>
2020-12-14 18:17:26 -0300

    Updates

---
+++ precache-manifest.6d276919e8321f2390eb014e62a39036.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa5978fe151289f6cfeaa7bc393e990",
+    "url": "/Platzi-badge/index.html"
+  },
+  {
+    "revision": "50e8620889e688e7b104",
+    "url": "/Platzi-badge/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "1eba2b5f5c54ce3e790f",
+    "url": "/Platzi-badge/static/css/main.7d5fa5c6.chunk.css"
+  },
+  {
+    "revision": "50e8620889e688e7b104",
+    "url": "/Platzi-badge/static/js/2.0dff5caf.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Platzi-badge/static/js/2.0dff5caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eba2b5f5c54ce3e790f",
+    "url": "/Platzi-badge/static/js/main.6fbdaed1.chunk.js"
+  },
+  {
+    "revision": "d99952e2f92324a0100f",
+    "url": "/Platzi-badge/static/js/runtime-main.95dc662a.js"
+  },
+  {
+    "revision": "2d8eb5e342327bf974b8d519780eb0b2",
+    "url": "/Platzi-badge/static/media/Twitter-Icon.2d8eb5e3.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Platzi-badge/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi-badge/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi-badge/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Platzi-badge/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi-badge/static/media/stars.1d17360a.svg"
+  }
+]);
